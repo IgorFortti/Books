5c2600fbd0c93ef14c647121a012f6a5d2a2f39d
Igor Fortti <igormfortti@gmail.com>
2023-09-18 13:57:48 -0300

    Refactory Loading

---
+++ Books/Ultils/Loading/Loading.swift
@@ -0,0 +1,31 @@
+//
+//  Loading.swift
+//  Books
+//
+//  Created by Igor Fortti on 18/09/23.
+//
+
+import UIKit
+
+class Loading {
+    
+    public static let shared = Loading()
+    
+    private init() {}
+    
+    func start(from view: UIView, isBackground: Bool = true, isLarge: Bool = true) {
+        let loading = LoadingView(parrentView: view, isBackground: isBackground, isLarge: isLarge)
+        loading.indicatorView.startAnimating()
+        view.addSubview(loading)
+        loading.setupView()
+    }
+    
+    func stop(from view: UIView) {
+        for view in view.subviews {
+            if let view = view as? LoadingView {
+                view.indicatorView.stopAnimating()
+                view.removeFromSuperview()
+            }
+        }
+    }
+}
